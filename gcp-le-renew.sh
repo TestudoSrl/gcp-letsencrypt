b4ef4607adf2e2ed78852fc7ca3220f2dee92a84
Francesco Cartier <francesco.cartier@testudosrl.com>
2021-11-19 13:49:18 +0100

    fix

--- gcp-le-renew.sh
+++ gcp-le-renew.sh
@@ -3,7 +3,7 @@
 # Restore working dir into /etc/letsencrypt/
 tar -zxf letsencrypt.tar.gz --directory /etc/letsencrypt/
 
-echo $$SA_KEY_FILE > certbot.json
+echo $SA_KEY_FILE > certbot.json
 
 cat certbot.json
 
